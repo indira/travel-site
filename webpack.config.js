c39d646f3c7fe8eec2b12e3af7778247d017b507
Indira <indubhusal@gmail.com>
2022-07-25 14:28:21 -0700

    Completed large hero block and intro content block

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const postCSSPlugins = [
+    require('postcss-import'),
+    require('postcss-mixins'),
+    require('postcss-nested'),
+    require('postcss-simple-vars'),
+    require('autoprefixer')
+]
+module.exports = {
+    entry: './app/assets/scripts/App.js',
+    output: {
+        filename: 'bundeled.js',
+        path:path.resolve(__dirname,'app')
+    },
+    devServer: {
+        watchFiles: ('./app/**/*.html'),
+        static: path.join(__dirname, 'app'),
+        hot: true,
+        port: 3000,
+        host:'0.0.0.0'
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use:['style-loader','css-loader',{loader:'postcss-loader',options:{postcssOptions:{plugins:postCSSPlugins}}}]
+            }
+        ]
+    }
+}
